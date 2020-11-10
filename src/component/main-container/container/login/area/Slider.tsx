5551dfdb74cfb8d728b4992f3a3287535284705b
kim's familly <zzanggar@kimsui-MacBookPro.local>
2020-11-10 16:11:25 +0900

    login area분리

--- src/component/main-container/container/login/area/Slider.tsx
+++ src/component/main-container/container/login/area/Slider.tsx
@@ -1,11 +1,9 @@
 import * as React from 'react'
 
-export default class MainContent extends React.Component<any, {}> {
+export default class Slider extends React.Component<any, {}> {
    render() {
       return <React.Fragment>
-         <main>
-            {/* Hero Area Start*/}
-            <div className="slider-area ">
+         <div className="slider-area ">
                <div className="single-slider slider-height2 d-flex align-items-center">
                   <div className="container">
                      <div className="row">
@@ -18,52 +16,6 @@ export default class MainContent extends React.Component<any, {}> {
                   </div>
                </div>
             </div>
-            {/* Hero Area End*/}
-            {/*================login_part Area =================*/}
-            <section className="login_part section_padding ">
-               <div className="container">
-                  <div className="row align-items-center">
-                     <div className="col-lg-6 col-md-6">
-                        <div className="login_part_text text-center">
-                           <div className="login_part_text_iner">
-                              <h2>New to our Shop?</h2>
-                              <p>There are advances being made in science and technology
-                      everyday, and a good example of this is the</p>
-                              <a href="#" className="btn_3">Create an Account</a>
-                           </div>
-                        </div>
-                     </div>
-                     <div className="col-lg-6 col-md-6">
-                        <div className="login_part_form">
-                           <div className="login_part_form_iner">
-                              <h3>Welcome Back ! <br />
-                      Please Sign in now</h3>
-                              <form className="row contact_form" action="#" method="post" >
-                                 <div className="col-md-12 form-group p_star">
-                                    <input type="text" className="form-control" id="name" name="name" placeholder="Username" />
-                                 </div>
-                                 <div className="col-md-12 form-group p_star">
-                                    <input type="password" className="form-control" id="password" name="password" placeholder="Password" />
-                                 </div>
-                                 <div className="col-md-12 form-group">
-                                    <div className="creat_account d-flex align-items-center">
-                                       <input type="checkbox" id="f-option" name="selector" />
-                                       <label htmlFor="f-option">Remember me</label>
-                                    </div>
-                                    <button type="submit" value="submit" className="btn_3">
-                                       log in
-                        </button>
-                                    <a className="lost_pass" href="#">forget password?</a>
-                                 </div>
-                              </form>
-                           </div>
-                        </div>
-                     </div>
-                  </div>
-               </div>
-            </section>
-            {/*================login_part end =================*/}
-         </main>
-      </React.Fragment>
+         </React.Fragment>
    }
 }
